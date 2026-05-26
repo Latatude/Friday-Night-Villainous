ab41bb1466b33a2376501fe3dd5efb76f910edfe
Veloz <elvelozmur6@gmail.com>
2026-05-26 15:39:51 -0300

    Eliminar 505-maid (2).lua

--- Friday Night Villainous/characters/505-maid (2).lua
+++
@@ -1,8 +0,0 @@
-function onUpdate(elapsed)
-    if mustHitSection == false then
-        setProperty('defaultCamZoom',0.7)
-    else
-       
-        setProperty('defaultCamZoom',1.0)
-    end
-end
